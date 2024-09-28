323e647050387458c4a66e7c8e323d74d7733043
babanat <babanat3@gmail.com>
2024-09-27 19:12:45 -0500

    Реализован просмотр фотографий в полноразмерном режиме

---
+++ js/bigPicture.js
@@ -0,0 +1,65 @@
+const bigPictureElement = document.querySelector('.big-picture');
+const bigPictureImg = bigPictureElement.querySelector('.big-picture__img img');
+const likesCount = bigPictureElement.querySelector('.likes-count');
+const commentsCount = bigPictureElement.querySelector('.comments-count');
+const commentsContainer = bigPictureElement.querySelector('.social__comments');
+const descriptionElement = bigPictureElement.querySelector('.social__caption');
+const commentTemplate = document.querySelector('#comment-template').content.querySelector('.social__comment');
+const bodyElement = document.querySelector('body');
+const closeButton = bigPictureElement.querySelector('.big-picture__cancel');
+
+function showBigPicture(photo) {
+  bigPictureElement.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+  bigPictureImg.src = photo.url;
+  likesCount.textContent = photo.likes;
+  commentsCount.textContent = photo.comments.length;
+  descriptionElement.textContent = photo.description;
+
+  renderComments(photo.comments);
+
+  bigPictureElement.querySelector('.social__comment-count').classList.add('hidden');
+  bigPictureElement.querySelector('.comments-loader').classList.add('hidden');
+
+  document.addEventListener('keydown', onDocumentKeyDown);
+}
+
+function renderComments(comments) {
+    commentsContainer.innerHTML = ''; 
+    const fragment = document.createDocumentFragment();
+  
+    comments.forEach((comment) => {
+      const commentElement = createCommentElement(comment);
+      fragment.appendChild(commentElement);
+    });
+  
+    commentsContainer.appendChild(fragment);
+  }
+  
+  // Функция для создания элемента комментария
+function createCommentElement(comment) {
+    const commentTemplate = document.querySelector('#comment-template').content.querySelector('.social__comment');
+    const commentElement = commentTemplate.cloneNode(true);
+  
+    commentElement.querySelector('.social__picture').src = comment.avatar;
+    commentElement.querySelector('.social__picture').alt = comment.name;
+    commentElement.querySelector('.social__text').textContent = comment.message;
+  
+    return commentElement;
+  }
+
+function closeBigPicture() {
+  bigPictureElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeyDown);
+}
+
+function onDocumentKeyDown(evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeBigPicture();
+  }
+}
+
+closeButton.addEventListener('click', closeBigPicture);
+export { showBigPicture };
--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 import { generatePhotos } from "./photo.js";
+import { showBigPicture } from "./bigPicture.js";
+import { addCommentTemplate } from './template.js';
 
 const picturesContainer = document.querySelector(".pictures");
 const pictureTemplate = document.querySelector("#picture")
 .content.querySelector(".picture");
 
+addCommentTemplate();
+
 function renderPhoto(photo) {
   const photoElement = pictureTemplate.cloneNode(true);
-
   photoElement.querySelector(".picture__img").src = photo.url;
   photoElement.querySelector(".picture__likes").textContent = photo.likes;
   photoElement.querySelector(".picture__comments").textContent =
     photo.comments.length;
 
+    photoElement.addEventListener('click', () => {
+      showBigPicture(photo);
+    });
+
   return photoElement;
 }
+
 
 function renderPhotos(photos) {
   const fragment = document.createDocumentFragment();
